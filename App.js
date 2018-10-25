4421de1fd33edbf56fc79a16df182355f1b8b66e
Nik Aglichev <nikaglichev@gmail.com>
2018-10-25 22:35:18 +0300

    Expo notigy work example

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //Try something intresting, the one more
 
-
+// https://expo.io/dashboard/notifications  - эта штука реально отправляет нотифы даже закрытые
 import * as React from 'react';
 import {
 	Text,
@@ -12,12 +12,12 @@ import {
 	TouchableOpacity,
 	Image,
 } from 'react-native'; 
-
+import {Notifications  } from 'expo';
 import { createStackNavigator, NavigationActions, withNavigation } from 'react-navigation';
 import { HomeScreen } from './components/HomeScreen';
 import { AssetExample } from './components/AssetExample';
 import BottomMenu from './components/BottomMenu';
-
+import registerForPushNotificationsAsync from './components/registerForPushNotificationsAsync';
 
 const Navigator_1 = createStackNavigator(
 	{
@@ -38,6 +38,23 @@ const Navigator_1 = createStackNavigator(
 );
 
 class App extends React.Component {
+
+
+	state = {
+		notification: {},
+	            };
+	          
+	            componentDidMount() {
+		registerForPushNotificationsAsync();
+
+		this._notificationSubscription = Notifications.addListener(this._handleNotification);
+	            }
+	          
+	            _handleNotification = (notification) => {
+		this.setState({notification: notification});
+	            };
+
+
 	constructor(props) {
 		super(props)
 		
@@ -52,6 +69,10 @@ class App extends React.Component {
 		return (
 			<View style={{ flex: 1 }}>
 				<Navigator_1 ref = { el  => { this.navig = el;  }} />
+				<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Origin: {this.state.notification.origin}</Text>
+        <Text>Data: {JSON.stringify(this.state.notification.data)}</Text>
+      </View>
 				<BottomMenu navigation={this.navigate}/>
 			</View>
 		)
